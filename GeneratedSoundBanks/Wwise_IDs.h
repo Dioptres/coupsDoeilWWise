f3a1e5fd40950fb20b809067ff08db8ae4806862
TristanPra <tristan.pradens@orange.fr>
2017-05-26 18:18:39 +0200

    wwise tout beau

---
+++ GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,75 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID AMBIANCE = 2981377429U;
+        static const AkUniqueID CHANTEUR_FIXE = 2843045782U;
+        static const AkUniqueID LAMPISTE_FIXE = 3239536015U;
+        static const AkUniqueID LAMPISTE_REGARD = 2109565432U;
+        static const AkUniqueID PLAY_CHANTEUR_REGARD = 2098483872U;
+        static const AkUniqueID PLAY_LAMPADAIRE = 4046997954U;
+        static const AkUniqueID STEP_RUMEUR = 1135652546U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace MUSICIENS
+        {
+            static const AkUniqueID GROUP = 1586184133U;
+
+            namespace STATE
+            {
+                static const AkUniqueID BASSE_JOUE = 1405321639U;
+                static const AkUniqueID CAJON_JOUE = 2467799850U;
+                static const AkUniqueID FLUTE_JOUE = 2415077747U;
+                static const AkUniqueID GUITARE2_JOUE = 2977724162U;
+                static const AkUniqueID GUITARE_JOUE = 846377368U;
+            } // namespace STATE
+        } // namespace MUSICIENS
+
+    } // namespace STATES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID MUSIC_VOLUME_OPTION = 3568425541U;
+        static const AkUniqueID SFX_VOLUME_OPTION = 3051629181U;
+        static const AkUniqueID SS_AIR_FEAR = 1351367891U;
+        static const AkUniqueID SS_AIR_FREEFALL = 3002758120U;
+        static const AkUniqueID SS_AIR_FURY = 1029930033U;
+        static const AkUniqueID SS_AIR_MONTH = 2648548617U;
+        static const AkUniqueID SS_AIR_PRESENCE = 3847924954U;
+        static const AkUniqueID SS_AIR_RPM = 822163944U;
+        static const AkUniqueID SS_AIR_SIZE = 3074696722U;
+        static const AkUniqueID SS_AIR_STORM = 3715662592U;
+        static const AkUniqueID SS_AIR_TIMEOFDAY = 3203397129U;
+        static const AkUniqueID SS_AIR_TURBULENCE = 4160247818U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID COUPDOEIL_SOUNDBANK = 4204588429U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MASTER_SECONDARY_BUS = 805203703U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID SFX = 393239870U;
+    } // namespace BUSSES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
