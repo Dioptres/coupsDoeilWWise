0e8fb2c37587c9b0f6048be10469b1b4ad4b3834
Dioptre <misiak.benjamin@gmail.com>
2017-05-31 18:45:51 +0200

    fix

--- GeneratedSoundBanks/Wwise_IDs.h
+++ GeneratedSoundBanks/Wwise_IDs.h
@@ -13,36 +13,133 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID AGITATEUR_MARCHE = 2688714084U;
+        static const AkUniqueID AGITATEUR_REGARD = 2449017055U;
         static const AkUniqueID AMBIANCE = 2981377429U;
+        static const AkUniqueID BASS_FILL = 3360699968U;
+        static const AkUniqueID BASS_MARCHE = 2271363949U;
         static const AkUniqueID CHANTEUR_FIXE = 2843045782U;
-        static const AkUniqueID LAMPISTE_FIXE = 3239536015U;
+        static const AkUniqueID CHANTEUR_REGARD = 852897365U;
+        static const AkUniqueID FLUTE_FILL = 1503509175U;
+        static const AkUniqueID FLUTE_MARCHE = 915394794U;
+        static const AkUniqueID FOULE = 4222617504U;
+        static const AkUniqueID GUIT_FILL = 3984850850U;
+        static const AkUniqueID GUIT_MARCHE = 3036075691U;
+        static const AkUniqueID LAMPADAIRE_FIREWORK = 2890891367U;
+        static const AkUniqueID LAMPADAIRE_OFF = 2841797443U;
+        static const AkUniqueID LAMPADAIRE_ON = 3295927151U;
+        static const AkUniqueID LAMPISTE_LUCIOLE = 232325074U;
+        static const AkUniqueID LAMPISTE_OFF = 2780761060U;
         static const AkUniqueID LAMPISTE_REGARD = 2109565432U;
-        static const AkUniqueID PLAY_CHANTEUR_REGARD = 2098483872U;
-        static const AkUniqueID PLAY_LAMPADAIRE = 4046997954U;
+        static const AkUniqueID LAMPISTE_TP = 32667521U;
+        static const AkUniqueID LAMPISTE_TP_LOAD = 553049592U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID RUMEUR_MARCHE = 3609129866U;
+        static const AkUniqueID RUMEUR_REGARD = 505317385U;
+        static const AkUniqueID RUMEUR_REVEIL = 1058187699U;
         static const AkUniqueID STEP_RUMEUR = 1135652546U;
+        static const AkUniqueID UKU_FILL = 679765766U;
+        static const AkUniqueID UKU_MARCHE = 2637819543U;
     } // namespace EVENTS
 
     namespace STATES
     {
-        namespace MUSICIENS
+        namespace BASS
         {
-            static const AkUniqueID GROUP = 1586184133U;
+            static const AkUniqueID GROUP = 1291433344U;
 
             namespace STATE
             {
-                static const AkUniqueID BASSE_JOUE = 1405321639U;
-                static const AkUniqueID CAJON_JOUE = 2467799850U;
-                static const AkUniqueID FLUTE_JOUE = 2415077747U;
-                static const AkUniqueID GUITARE2_JOUE = 2977724162U;
-                static const AkUniqueID GUITARE_JOUE = 846377368U;
+                static const AkUniqueID BASS_ISPLAYING = 3086564927U;
+                static const AkUniqueID BASS_NOTPLAYING = 3365119364U;
             } // namespace STATE
-        } // namespace MUSICIENS
+        } // namespace BASS
+
+        namespace FLUTE
+        {
+            static const AkUniqueID GROUP = 1167860281U;
+
+            namespace STATE
+            {
+                static const AkUniqueID FLUTE_ISPLAYING = 3176663318U;
+                static const AkUniqueID FLUTE_NOTPLAYING = 4227903807U;
+            } // namespace STATE
+        } // namespace FLUTE
+
+        namespace GUITARE
+        {
+            static const AkUniqueID GROUP = 703172684U;
+
+            namespace STATE
+            {
+                static const AkUniqueID GUITARE_ISPLAYING = 1877199627U;
+                static const AkUniqueID GUITARE_NOTPLAYING = 3436554448U;
+            } // namespace STATE
+        } // namespace GUITARE
+
+        namespace PERCU
+        {
+            static const AkUniqueID GROUP = 240420578U;
+
+            namespace STATE
+            {
+                static const AkUniqueID PERCU_INTRO = 581627101U;
+                static const AkUniqueID PERCU_JEU = 1739665607U;
+            } // namespace STATE
+        } // namespace PERCU
+
+        namespace STATE_ANCIENNEMETHODE
+        {
+            static const AkUniqueID GROUP = 3520505652U;
+
+            namespace STATE
+            {
+                static const AkUniqueID BASS_FLUTE = 3280668067U;
+                static const AkUniqueID BASS_FLUTE_UKU = 811853599U;
+                static const AkUniqueID BASS_GUITARE_FLUTE = 825017421U;
+                static const AkUniqueID BASS_UKU = 2548343116U;
+                static const AkUniqueID ENSEMBLE = 3112702240U;
+                static const AkUniqueID GUITARE_BASS = 3275671058U;
+                static const AkUniqueID GUITARE_BASS_UKULELE = 1672548934U;
+                static const AkUniqueID GUITARE_FLUTE = 3337521423U;
+                static const AkUniqueID GUITARE_FLUTE_UKU = 2242863827U;
+                static const AkUniqueID GUITARE_UKU = 3724199040U;
+                static const AkUniqueID PERCU = 240420578U;
+                static const AkUniqueID UKU_FLUTE = 2366198309U;
+            } // namespace STATE
+        } // namespace STATE_ANCIENNEMETHODE
+
+        namespace UKU
+        {
+            static const AkUniqueID GROUP = 1147835290U;
+
+            namespace STATE
+            {
+                static const AkUniqueID UKU_ISPLAYING = 1825451673U;
+                static const AkUniqueID UKU_NOTPLAYING = 1337463558U;
+            } // namespace STATE
+        } // namespace UKU
 
     } // namespace STATES
 
+    namespace SWITCHES
+    {
+        namespace MUSIC
+        {
+            static const AkUniqueID GROUP = 3991942870U;
+
+            namespace SWITCH
+            {
+            } // namespace SWITCH
+        } // namespace MUSIC
+
+    } // namespace SWITCHES
+
     namespace GAME_PARAMETERS
     {
+        static const AkUniqueID FOULE = 4222617504U;
         static const AkUniqueID MUSIC_VOLUME_OPTION = 3568425541U;
+        static const AkUniqueID MUSICIENS = 1586184133U;
         static const AkUniqueID SFX_VOLUME_OPTION = 3051629181U;
         static const AkUniqueID SS_AIR_FEAR = 1351367891U;
         static const AkUniqueID SS_AIR_FREEFALL = 3002758120U;
